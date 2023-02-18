9b6f344d7750392cea10d92f62ea215be66246e2
Michał Szczęsny <73541429+Dog1e@users.noreply.github.com>
2023-02-18 16:37:04 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Board from "./components/Board/board";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
